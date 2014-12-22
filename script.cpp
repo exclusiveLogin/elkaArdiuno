cd900f252e010ef2a4530718d3ae9fb32a033572
exclusiveLogin <alter4444@gmail.com>
2014-12-22 20:39:36 +0400

    refactoring code

--- script.cpp
+++ script.cpp
@@ -4,32 +4,43 @@ developer:Sergey Savin
 Kolibri Media Holding
 */
 //Global SECTION
-
+int pinsRadial[5]={2,3,4,5,6};
+int pinsHorizontAll[15]={22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
+int pinsHorizontRed[5]={22,23,24,25,26};
+int pinsHorizontGreen[5]={27,28,29,30,31};
+int pinsHorizontBlue[5]={32,33,34,35,36};
+int pinsStars[4]={8,9,10,11};
+int pinStart = 19;//кнопка приветствия Елочка гори.
+int pinCyrcle = 20;//кнопка включения стека паттернов
+int pinStop =21;//кнопка полного останова
+int pinMainStar = 52;
+int pinIceFall = 51;
 //--------PRIMITIVES-----------------
 //--------Radial---------------------
 class Radial{
   public:
-    Radial(int pins[5]);
+    Radial(int pins[(sizeof(pinsRadial)/sizeof(*pinsRadial))]);
     void setNumOfRay(int num_of_rays);
     void setDirection(boolean dir);
     void Step();
     void Blink();
     void Stop();
+    void Reset();
   private:
     void lightMaping();
     int pinID[5];
     int numRaysVal;
     int stepVal;//max 5
-    boolean lightMap[5];
-    boolean realLight[5];
+    boolean lightMap[(sizeof(pinsRadial)/sizeof(*pinsRadial))];
+    boolean realLight[(sizeof(pinsRadial)/sizeof(*pinsRadial))];
 };
-Radial::Radial(int pins[5]){
+Radial::Radial(int pins[(sizeof(pinsRadial)/sizeof(*pinsRadial))]){
   stepVal=1;//max 5
-  for(int i=0;i<5;i++){
+  for(int i=0;i<(sizeof(pinsRadial)/sizeof(*pinsRadial));i++){
     pinID[i]=pins[i];
     pinMode(pinID[i], OUTPUT);
   }
-  numRaysVal = 1;
+  numRaysVal = 3;
   stepVal=1;
   lightMaping();
 }
@@ -41,7 +52,7 @@ void Radial::setNumOfRay(int num_of_rays){
 void Radial::lightMaping(){
   int counter = numRaysVal;
   boolean state = true;
-  for(int i=0;i<5;i++){
+  for(int i=0;i<(sizeof(pinsRadial)/sizeof(*pinsRadial));i++){
     lightMap[i]=state;
     counter--;
     if(counter==0){
@@ -51,65 +62,73 @@ void Radial::lightMaping(){
   }
 }
 void Radial::Step(){
-  if(stepVal>5)stepVal=1;
-  for(int i=0;i<5;i++){//перенос матрицы
-    if((stepVal+i)>5){
-      realLight[(stepVal+i)-6] = lightMap[i];
+  if(stepVal>(sizeof(pinsRadial)/sizeof(*pinsRadial)))stepVal=1;
+  for(int i=0;i<(sizeof(pinsRadial)/sizeof(*pinsRadial));i++){//перенос матрицы
+    if((stepVal+i)>(sizeof(pinsRadial)/sizeof(*pinsRadial))){
+      realLight[(stepVal+i-1)-(sizeof(pinsRadial)/sizeof(*pinsRadial))] = lightMap[i];
     }
     else{
       realLight[stepVal+i-1] = lightMap[i];
     }    
   }  
-  for(int i=0;i<5;i++){//Активация апаратной части
+  for(int i=0;i<(sizeof(pinsRadial)/sizeof(*pinsRadial));i++){//Активация апаратной части
     digitalWrite(pinID[i], realLight[i]);
   }
   stepVal++;
 }
 void Radial::Blink(){  
-  for(int i=0;i<5;i++){
+  for(int i=0;i<(sizeof(pinsRadial)/sizeof(*pinsRadial));i++){
     lightMap[i]=!lightMap[i];
   }
-  for(int i=0;i<5;i++){//Активация апаратной части
+  for(int i=0;i<(sizeof(pinsRadial)/sizeof(*pinsRadial));i++){//Активация апаратной части
     digitalWrite(pinID[i], lightMap[i]);
   }
 }
 void Radial::Stop(){
-  for(int i=0;i<5;i++){//Активация апаратной части
+  for(int i=0;i<(sizeof(pinsRadial)/sizeof(*pinsRadial));i++){//Активация апаратной части
     digitalWrite(pinID[i], LOW);
   }
+}
+void Radial::Reset(){
+  Stop();
+  numRaysVal = 1;
+  stepVal=1;
+  lightMaping();
 }
 //------------HORIZONT------------------------
 class Horizont{
   public:
-    Horizont(int pins[9],int pinsR[3],int pinsG[3],int pinsB[3]);
+    Horizont(int pins[(sizeof(pinsHorizontAll)/sizeof(*pinsHorizontAll))],int pinsR[(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed))],int pinsG[(sizeof(pinsHorizontGreen)/sizeof(*pinsHorizontGreen))],int pinsB[(sizeof(pinsHorizontBlue)/sizeof(*pinsHorizontBlue))]);
     void setColor(int color);
     void setMode(boolean mode);
     void setDirection(boolean dir);
     void Step();
     void Dance();
     void Stop();
+    void Reset();
   private:
     int PColor;
     boolean PFill;
     boolean PDirection;
-    int PPinID[9];
-    int PPinRed[3];
-    int PPinGreen[3];
-    int PPinBlue[3];
+    int PPinID[(sizeof(pinsHorizontAll)/sizeof(*pinsHorizontAll))];
+    int PPinRed[(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed))];
+    int PPinGreen[(sizeof(pinsHorizontGreen)/sizeof(*pinsHorizontGreen))];
+    int PPinBlue[(sizeof(pinsHorizontBlue)/sizeof(*pinsHorizontBlue))];
     int stepVal;//max 3
-    boolean lightMap[3];
+    boolean lightMap[(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed))];
 };
-Horizont::Horizont(int pins[9],int pinsR[3],int pinsG[3],int pinsB[3]){
-  for(int i=0;i<9;i++){
+Horizont::Horizont(int pins[(sizeof(pinsHorizontAll)/sizeof(*pinsHorizontAll))],int pinsR[(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed))],int pinsG[(sizeof(pinsHorizontGreen)/sizeof(*pinsHorizontGreen))],int pinsB[(sizeof(pinsHorizontBlue)/sizeof(*pinsHorizontBlue))]){
+  for(int i=0;i<(sizeof(pinsHorizontAll)/sizeof(*pinsHorizontAll));i++){
     PPinID[i]=pins[i];
+    pinMode(PPinID[i],OUTPUT);
   }
-  for(int i=0;i<3;i++){
+  for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){
     PPinRed[i]=pinsR[i];
   }
-  for(int i=0;i<3;i++){
+  for(int i=0;i<(sizeof(pinsHorizontGreen)/sizeof(*pinsHorizontGreen));i++){
     PPinGreen[i]=pinsG[i];
   }
-  for(int i=0;i<3;i++){
+  for(int i=0;i<(sizeof(pinsHorizontBlue)/sizeof(*pinsHorizontBlue));i++){
     PPinBlue[i]=pinsB[i];
   }
   PColor=7;
@@ -127,14 +146,14 @@ void Horizont::setMode(boolean mode){
   PFill=mode;//false=single true=solid fill
 }
 void Horizont::Step(){
-  if(PDirection==true&&stepVal>3)stepVal=1;
-  if(PDirection==false&&stepVal<1)stepVal=3;
+  if(PDirection==true&&stepVal>(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed)))stepVal=1;
+  if(PDirection==false&&stepVal<1)stepVal=(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));
   
   if(!PFill){//Без заполнения
-    for(int i=0;i<3;i++){
+    for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){
       lightMap[i]=false;
     }    
-    for(int i=0; i<3; i++){
+    for(int i=0; i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed)); i++){
       if(stepVal==i+1){
         lightMap[i]=true;
       }
@@ -142,7 +161,7 @@ void Horizont::Step(){
     }
   }
   else{//С заполнением
-    for(int i=0; i<3; i++){
+    for(int i=0; i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed)); i++){
       if(stepVal==i+1){
         lightMap[i]=!lightMap[i];
       }
@@ -152,7 +171,7 @@ void Horizont::Step(){
   
   //Render
   Stop();
-  for(int i=0;i<3;i++){
+  for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){
     Serial.print(lightMap[i]);
     
   }
@@ -162,28 +181,28 @@ void Horizont::Step(){
   
   switch(PColor){
     case 1:  
-      for(int i=0;i<3;i++){//красные
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){//красные
       if(lightMap[i]){
         digitalWrite(PPinRed[i], HIGH);
         }
       }
       break;
     case 2:    
-      for(int i=0;i<3;i++){//зеленые
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){//зеленые
       if(lightMap[i]){
         digitalWrite(PPinGreen[i], HIGH);
         }
       }
       break;
     case 3:  
-      for(int i=0;i<3;i++){//синие
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){//синие
       if(lightMap[i]){
         digitalWrite(PPinBlue[i], HIGH);
         }
       }  
       break;
     case 4:  
-      for(int i=0;i<3;i++){//красные + зеленые
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){//красные + зеленые
       if(lightMap[i]){
         digitalWrite(PPinRed[i], HIGH);
         digitalWrite(PPinGreen[i], HIGH);
@@ -191,7 +210,7 @@ void Horizont::Step(){
       }
       break;
     case 5: 
-      for(int i=0;i<3;i++){//красные + синии
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){//красные + синии
       if(lightMap[i]){
         digitalWrite(PPinRed[i], HIGH);
         digitalWrite(PPinBlue[i], HIGH);
@@ -199,7 +218,7 @@ void Horizont::Step(){
       }
       break;
     case 6:  
-      for(int i=0;i<3;i++){//синии + зеленые
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){//синии + зеленые
       if(lightMap[i]){
         digitalWrite(PPinBlue[i], HIGH);
         digitalWrite(PPinGreen[i], HIGH);
@@ -207,7 +226,7 @@ void Horizont::Step(){
       }
       break;
     case 7:  
-      for(int i=0;i<3;i++){//красные + зеленые + синии
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){//красные + зеленые + синии
       if(lightMap[i]){
         digitalWrite(PPinRed[i], HIGH);
         digitalWrite(PPinGreen[i], HIGH);
@@ -223,44 +242,45 @@ void Horizont::Step(){
 }
 
 void Horizont::Dance(){
+  Stop();
   if(PDirection==true&&stepVal>7)stepVal=1;
   if(PDirection==false&&stepVal<1)stepVal=7;
   switch(stepVal){
     case 1://All Red
-      for(int i=0;i<3;i++){ 
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){ 
         digitalWrite(PPinRed[i], HIGH);
       }
       break;
     case 2://All Green
-      for(int i=0;i<3;i++){
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){
         digitalWrite(PPinGreen[i], HIGH);
       }
       break;
     case 3://All Blue
-      for(int i=0;i<3;i++){ 
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){ 
         digitalWrite(PPinBlue[i], HIGH);
       }
       break;
     case 4://Red+Green
-      for(int i=0;i<3;i++){ 
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){ 
         digitalWrite(PPinRed[i], HIGH);
         digitalWrite(PPinGreen[i], HIGH);
       }
       break;
     case 5://Green+Blue
-      for(int i=0;i<3;i++){ 
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){ 
         digitalWrite(PPinGreen[i], HIGH);
         digitalWrite(PPinBlue[i], HIGH);
       }
       break;
     case 6://Red+Blue
-      for(int i=0;i<3;i++){
+      for(int i=0;i<(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));i++){
         digitalWrite(PPinRed[i], HIGH);
         digitalWrite(PPinBlue[i], HIGH);
       }
       break;
     case 7://All
-      for(int i=0;i<9;i++){
+      for(int i=0;i<(sizeof(pinsHorizontAll)/sizeof(*pinsHorizontAll));i++){
         digitalWrite(PPinID[i], HIGH);
       }
       break; 
@@ -272,9 +292,15 @@ void Horizont::Dance(){
   else stepVal--;
 }
 void Horizont::Stop(){
-  for(int i=0;i<9;i++){//обнуляем все
+  for(int i=0;i<(sizeof(pinsHorizontAll)/sizeof(*pinsHorizontAll));i++){//обнуляем все
     digitalWrite(PPinID[i], LOW);
   }
+}
+void Horizont::Reset(){
+  PColor=7;
+  PFill=false;
+  PDirection=true;
+  stepVal=1;
 }
 //------------MainStar-----------------
 class MainStar{
@@ -362,14 +388,14 @@ void Stars::setMode(boolean mode){
   PFill=mode;//false=single true=solid fill
 }
 void Stars::Step(){
-  if(PDirection==true&&stepVal>12)stepVal=1;
-  if(PDirection==false&&stepVal<1)stepVal=12;
+  if(PDirection==true&&stepVal>(sizeof(pinsStars)/sizeof(*pinsStars)))stepVal=1;
+  if(PDirection==false&&stepVal<1)stepVal=(sizeof(pinsStars)/sizeof(*pinsStars));
   
   if(!PFill){//Без заполнения
-    for(int i=0;i<12;i++){
+    for(int i=0;i<(sizeof(pinsStars)/sizeof(*pinsStars));i++){
       lightMap[i]=false;
     }    
-    for(int i=0; i<12; i++){
+    for(int i=0; i<(sizeof(pinsStars)/sizeof(*pinsStars)); i++){
       if(stepVal==i+1){
         lightMap[i]=true;
       }
@@ -377,7 +403,7 @@ void Stars::Step(){
     }
   }
   else{//С заполнением
-    for(int i=0; i<12; i++){
+    for(int i=0; i<(sizeof(pinsStars)/sizeof(*pinsStars)); i++){
       if(stepVal==i+1){
         lightMap[i]=!lightMap[i];
       }
@@ -388,7 +414,7 @@ void Stars::Step(){
   //Render
   Stop();  
   
-  for(int i=0;i<12;i++){
+  for(int i=0;i<(sizeof(pinsStars)/sizeof(*pinsStars));i++){
    if(lightMap[i]){
      digitalWrite(PPinID[i], HIGH);
     }
@@ -400,16 +426,16 @@ void Stars::Step(){
   else stepVal--;  
 }
 void Stars::Blink(){
-  if(PDirection==true&&stepVal>12)stepVal=1;
-  if(PDirection==false&&stepVal<1)stepVal=12;
+  if(PDirection==true&&stepVal>(sizeof(pinsStars)/sizeof(*pinsStars)))stepVal=1;
+  if(PDirection==false&&stepVal<1)stepVal=(sizeof(pinsStars)/sizeof(*pinsStars));
   
   if(stepVal%2==0){
-    for(int i=0;i<12;i++){//обнуляем все
+    for(int i=0;i<(sizeof(pinsStars)/sizeof(*pinsStars));i++){//обнуляем все
     digitalWrite(PPinID[i], LOW);
     }
   }
   else{
-    for(int i=0;i<12;i++){
+    for(int i=0;i<(sizeof(pinsStars)/sizeof(*pinsStars));i++){
     digitalWrite(PPinID[i], HIGH);
     }
   }
@@ -418,18 +444,19 @@ void Stars::Blink(){
   else stepVal--;  
 }
 void Stars::Start(){
-  for(int i=0;i<12;i++){//обнуляем все
+  for(int i=0;i<(sizeof(pinsStars)/sizeof(*pinsStars));i++){//обнуляем все
     digitalWrite(PPinID[i], HIGH);
   }
 }
 
 void Stars::Stop(){
-  for(int i=0;i<12;i++){//обнуляем все
+  for(int i=0;i<(sizeof(pinsStars)/sizeof(*pinsStars));i++){//обнуляем все
     digitalWrite(PPinID[i], LOW);
-    //stepVal=1;
   }
 }
 void Stars::Reset(){
+  PFill=false;
+  PDirection=true;
   stepVal=1;
 }
 //-------------SUBTREES------------------------
@@ -490,149 +517,37 @@ void SubTrees::Stop(){
 //--------------------------------------------------
 //--------PRIMITIVES END----------------------------
 
-int pinsRadial[5]={3,4,5,6,7};
-int pinsHorizontAll[9]={22,23,24,25,26,27,28,29,30};
-int pinsHorizontRed[9]={22,25,28};
-int pinsHorizontGreen[9]={23,26,29};
-int pinsHorizontBlue[9]={24,27,30};
-int pinsStars[12]={31,32,33,34,35,36,37,38,39,40,41,42};
 Radial rad(pinsRadial);
 Horizont hor(pinsHorizontAll,pinsHorizontRed,pinsHorizontGreen,pinsHorizontBlue);
 Stars stars(pinsStars);
+IceFall ice(pinIceFall);
+MainStar mainstar(pinMainStar);
 
-void PatternOne(){
-  stars.setMode(true);
-  for(int i=0; i<1000; i++){
-    stars.Step();
-    delay(100);
+int GlobalMode=0;//0-test 1-standby 2-start 3-cyrcle
+
+void FullTest(){
+  //Сюда делаем полный перебор каналов
+  Serial.println(sizeof(pinsRadial)/sizeof(*pinsRadial));
+  Serial.println(sizeof(pinsHorizontAll)/sizeof(*pinsHorizontAll));
+  Serial.println(sizeof(pinsHorizontRed)/sizeof(*pinsHorizontRed));
+  Serial.println(sizeof(pinsHorizontGreen)/sizeof(*pinsHorizontGreen));
+  Serial.println(sizeof(pinsHorizontBlue)/sizeof(*pinsHorizontBlue));
+  Serial.println(sizeof(pinsStars)/sizeof(*pinsStars));
+
+  for(int i=0;i<100;i++){
+    ice.Blink();
+    mainstar.Blink();
+    delay(200);    
   }
-  stars.Stop();
-  
-  
-  
-/*  
-  rad.setNumOfRay(1);
-  for(int i=0; i<10; i++){
-    rad.Step();
-    delay(250);
-  }
-  rad.Stop();
-//-----
-  hor.setDirection(false);
-  hor.setMode(true);
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(1);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(2);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(3);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(4);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(5);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(6);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(7);
-//-------------------------  
-  hor.setMode(false);
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(1);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(2);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(3);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(4);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(5);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(6);
-  
-  for(int i=0; i<30; i++){
-    hor.Step();
-    delay(150);
-  }
-  hor.Stop();
-  hor.setColor(7);*/
-  
-  
-  //Dance
-  /*for(int i=0; i<100; i++){
-    hor.Dance();    
-    delay(500);
-    hor.Stop();
-  }*/
-  
+  ice.Stop();
+  mainstar.Stop();
 }
+
 
 void setup(){
   Serial.begin(9600);
   pinMode(13, OUTPUT);  
-  PatternOne();
+  FullTest();
 }
 void loop(){
   digitalWrite(13, HIGH);
